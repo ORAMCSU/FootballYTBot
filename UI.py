8b2503f6a2419fff46ee750cd68bd414f17d5f3d
Julien M <julien.marzal@telecom-paris.fr>
2021-03-12 21:03:30 +0100

    Manager with links done

--- UI.py
+++ UI.py
@@ -9,27 +9,55 @@ class ManagerWindow(Tk):
 
         Tk.__init__(self)
         self.title("Stream Manager")
+        self.old_number = 0
+        self.url_entries = []
 
         self.MainFrame = Frame(self, width=500, height=500)
         self.MatchButton = Button(self.MainFrame, text="Lancer le suivi", command=self.launch_match)
+        self.NumberRoll = Spinbox(self.MainFrame, from_=1, to=4)
+        self.NumberButton = Button(self.MainFrame, text="Valider", command=self.generate_urls)
 
         self.MainFrame.grid(row=0, column=0)
-        self.MatchButton.grid(row=0, column=0)
+        Label(self.MainFrame, text="Nombre de matches").grid(row=0, column=0)
+        self.NumberRoll.grid(row=0, column=1)
+        self.NumberButton.grid(row=0, column=2)
 
         self.MatchWindow = None
 
+        self.generate_urls()
+
     def launch_match(self, _event=None):
 
-        self.MatchWindow = MatchWindow(self)
+        self.MatchWindow = MatchWindow(master=self, nb_matches=int(self.NumberRoll.get()), url_list=[i.get() for
+                                       i in self.url_entries])
+
+    def generate_urls(self, _event=None):
+
+        number = int(self.NumberRoll.get())
+        if self.old_number != number:
+            self.MatchButton.grid_forget()
+            if number > self.old_number:
+                for i in range(number-self.old_number):
+                    self.url_entries.append(Entry(self.MainFrame))
+                    self.url_entries[self.old_number+i].grid(row=self.old_number+1+i, column=1)
+            else:
+                for i in range(self.old_number-number):
+                    self.url_entries[number+i].destroy()
+                self.url_entries = self.url_entries[:number]
+
+            self.old_number = number
+
+            self.MatchButton.grid(row=self.old_number+1, column=1)
 
 
 class MatchWindow(Toplevel):
 
-    def __init__(self, url_list, video_url=None, master=None, nb_matches=1):
+    def __init__(self, master=None, nb_matches=1, url_list=None, video_url=None):
 
         Toplevel.__init__(self, master)
         self.title("Match Stream")
         self.match_urls = url_list
+        print(self.match_urls)
 
         self.video_url = video_url
         self.MatchCanvas = Canvas(self, width=1536, height=864)
@@ -68,7 +96,8 @@ class MatchWindow(Toplevel):
         elif nb_matches == 3 or nb_matches == 4:
             pil_image2 = pil_image.resize((700, 156))
         pil_image.close()
-        self.displayed_black = PIL.ImageTk.PhotoImage(pil_image2)
+        if pil_image2:
+            self.displayed_black = PIL.ImageTk.PhotoImage(pil_image2)
 
         for i in range(nb_matches):
             if nb_matches == 1:
@@ -96,3 +125,6 @@ class MatchWindow(Toplevel):
             self.MatchCanvas.create_image(1341, 45 + 70 * i, image=self.displayed_icons[i], tag="Icon" + str(i))
 
         pil_image2.close()
+
+    def load_match_stats(self):
+        pass
